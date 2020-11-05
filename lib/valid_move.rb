820d31994ff0c69baf3ddf48c82cb64a620494e6
Jonathan Mattos <jonmattos2014@gmail.com>
2020-11-05 03:40:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,15 +3,13 @@
 def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index)
       return true
-      
-    else
-       index.between?(0,8) && !position_taken?(board, index)
-      return false
    end
 end
 
 
 
 def position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+      return false
   
 end
